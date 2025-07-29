2d2927a9098b75e6a3cc24bdf90d1a24117cdc1e
simon8491 <chmel.simon@gmail.com>
2025-07-29 19:27:34 +0200

    Init commit

---
+++ src/main/java/com/ingredientchanger/IngredientChangerApplication.java
@@ -0,0 +1,13 @@
+package com.ingredientchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientChangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientChangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingredientchanger/IngredientChangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingredientchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientChangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
