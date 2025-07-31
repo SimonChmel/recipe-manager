cfbe5ade49fc0065d46ff49eeb6f5bd2b8869531
simon8491 <chmel.simon@gmail.com>
2025-07-31 17:47:36 +0200

    project adjustment

--- src/main/java/com/recipemanager/IngredientChangerApplication.java
+++ src/main/java/com/recipemanager/IngredientChangerApplication.java
@@ -1,4 +1,4 @@
-package com.ingredientchanger;
+package com.recipemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/recipemanager/ServletInitializer.java
+++ src/main/java/com/recipemanager/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ingredientchanger;
+package com.recipemanager;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
